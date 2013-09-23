ae8c0287d78c043c6250e90c723ff7bd2e4c8425
Sally Carter <sacarter10@gmail.com>
2013-09-22 17:54:31 -0700

    remove extraneous print statement

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,6 @@ class User < ActiveRecord::Base
     :source => :book
 
   def self.from_omniauth(auth) 
-    p "!!!!!!!!! #{auth}"
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
